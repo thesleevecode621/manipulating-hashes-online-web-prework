983aa043e6fb637fb0114bc68652ff83d2952e34
boruch d <dubywider@gmail.com>
2019-04-14 19:22:12 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,12 +1,13 @@
 def first_challenge
   contacts = {
-    "Jon Snow" => {
-      name: "Jon",
-      email: "jon_snow@thewall.we", 
-      favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
-      knows: nil
-    },
-    Freddy Mercury: 
+    Jon Snow:      
+{ :name=>"Jon", 
+  :email=>"jon_snow@thewall.we", 
+  :favorite_icecream_flavors=>["chocolate", "vanilla", "mint chip"],
+  :knows=>nil
+}
+ 
+Freddy Mercury: 
 { :name=>"Freddy", 
 :email=>"freddy@mercury.com", 
 :favorite_icecream_flavors=>["strawberry", "cookie dough", "mint chip"]
