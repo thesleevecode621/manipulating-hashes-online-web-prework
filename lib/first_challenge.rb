6d4deb8398e9a21cac384d4f9d6f6cbc038959e4
boruch d <dubywider@gmail.com>
2019-04-14 19:32:29 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,12 +1,16 @@
-def first_challenge
- contacts = {
+contacts = {
   "Jon Snow" => {
     name: "Jon",
     email: "jon_snow@thewall.we", 
     favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
     knows: nil
   },
- }
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  }
+}
 
     
 contacts.each do |person, data|
