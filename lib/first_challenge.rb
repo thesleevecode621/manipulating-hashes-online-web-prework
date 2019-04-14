f24b2e9969b3fdbf9caf5bb7916f5667d13ada4a
boruch d <dubywider@gmail.com>
2019-04-14 19:12:12 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,7 +12,17 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
+contacts.each do |person, data|
+    data.each do |attr, value|
+      if attr == :favorite_icecream_flavors
+        value.delete_if { |flavor| flavor == "strawberry" }
+      end
+    end
+  end
+  
+  contacts
+  
+end
   #your code here
 
 
